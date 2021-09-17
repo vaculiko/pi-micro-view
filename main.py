37d9f83d95e15609e0533d19d0dada914b8ec28b
Ondrej Vaculik <vacuon@isibrno.cz>
2021-09-17 19:51:18 +0200

    Added 4 button keyboard

--- main.py
+++ main.py
@@ -6,38 +6,62 @@ from picamera import PiCamera
 from datetime import datetime
 
 # https://roboticadiy.com/how-to-debounce-push-button-in-raspberry-pi-4/
-button = Button(2)
+button1 = Button(25)
+button2 = Button(8)
+button3 = Button(7)
+button4 = Button(1)
+
 camera = PiCamera()
 camera.resolution = (1920, 1080)
 camera.framerate = 30
 
 
 def set_saving_directory():
-    drive_name = os.listdir('/media/pi')
+    drive_name = os.listdir("/media/pi")
     if drive_name == []:
-        os.chdir('/home/pi/Desktop')
+        os.chdir("/home/pi/Desktop")
     else:
-        os.chdir('/media/pi/' + drive_name[0])
+        os.chdir("/media/pi/" + drive_name[0])
 
 
 def take_picture():
     date_string = datetime.now().strftime("%Y%m%d-%H%M%S")
-    print(f'Image {date_string} captured')
-    camera.annotate_text = ''
+    print(f"Image {date_string} captured")
+    camera.annotate_text = ""
     set_saving_directory()
-    camera.capture(f'image-{date_string}.png', use_video_port=True)
-    camera.annotate_text = f'Image {date_string} saved to {os.getcwd()}!'
-    sleep(.1)
+    camera.capture(f"image-{date_string}.png", use_video_port=True)
+    camera.annotate_text = f"Image {date_string} saved to {os.getcwd()}!"
+    sleep(0.1)
 
 
-def shutdown():
+def zoom_preview():
+    no_zoom = (0.0, 0.0, 1.0, 1.0)
+    zoom2x = (0.25, 0.25, 0.75, 0.75)
+    if camera.zoom() == no_zoom:
+        camera.zoom(zoom2x)
+        camera.annotate_text = "Zoom: 2x"
+    else:
+        camera.zoom(no_zoom)
+        camera.annotate_text = "Zoom: 0x"
+
+
+def exposure_lock():
+    if camera.exposure_mode() == "auto":
+        camera.exposure_mode("off")
+    else:
+        camera.exposure_mode("auto")
+    camera.annotate_text = f"Exposure mode {camera.exposure_mode()}"
+
+
+def stop():
     camera.stop_preview()
     camera.close()
     quit()
-    
+
 
 camera.start_preview()
-camera.annotate_text = f'Resolution {camera.resolution}'
-button.when_held = shutdown
-button.when_pressed = take_picture
-pause()
+camera.annotate_text = f"Resolution {camera.resolution}"
+button1.when_pressed = take_picture
+button2.when_pressed = zoom_preview
+# button3.when_pressed = take_picture
+button4.when_pressed = stop
