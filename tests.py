9e73bed12971337a3d44ebb697ae892243b32544
Ondrej Vaculik <vacuon@isibrno.cz>
2021-09-17 22:39:34 +0200

    Basic HW tests

---
+++ tests.py
@@ -0,0 +1,8 @@
+from main import *
+
+print("All libraries imported successfully!")
+print("Press any button:")
+button1.when_pressed = print("Button1")
+button2.when_pressed = print("Button2")
+button3.when_pressed = print("Button3")
+button4.when_pressed = print("Button4")
