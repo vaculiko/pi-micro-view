8bd4503dad7e0d18aa2367204cea550928ee73c9
Ondrej Vaculik <vacuon@isibrno.cz>
2021-09-27 17:02:09 +0200

    Autorun setup

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 echo "Enable camera"
-sudo raspi-config nonint do_camera 0
+raspi-config nonint do_camera 0
 
 echo "Increasing GPU memory to 512"
 sed -i "s/^gpu_mem.*/gpu_mem=512/" /boot/config.txt
 
 echo "Installing requirements for pi-micro-view"
 pip3 install -r requirements.txt
+
+echo "Setup script autorun"
+mkdir /home/pi/.config/autostart
+cp pi-micro-view.desktop /home/pi/.config/autostart/
+
+echo "Finished, rebooting..."
+sudo reboot
