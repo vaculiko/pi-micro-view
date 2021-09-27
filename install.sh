718ac37c90c583296cd441e92dffa66bcb71873f
Ondrej Vaculik <vacuon@isibrno.cz>
2021-09-27 16:40:03 +0200

    Enable camera and increase gpu memory

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Install requirements for pi-micro-view"
+echo "Enable camera"
+sudo raspi-config nonint do_camera 0
 
+echo "Increasing GPU memory to 512"
+sed -i "s/^gpu_mem.*/gpu_mem=512/" /boot/config.txt
+
+echo "Installing requirements for pi-micro-view"
 pip3 install -r requirements.txt
