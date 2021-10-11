9b22acd190b1900fd01675d854854f6846578de7
Ondřej Vaculík <35863250+vaculiko@users.noreply.github.com>
2021-10-11 16:46:26 +0200

    Disable screensaver

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ raspi-config nonint do_camera 0
 echo "Increasing GPU memory to 512"
 sed -i "s/^gpu_mem.*/gpu_mem=512/" /boot/config.txt
 
+echo "Disabling screen blackout"
+sudo bash -c "echo "consoleblank=0" >> /boot/cmdline.txt"
+
 echo "Installing requirements for pi-micro-view"
 pip3 install -r requirements.txt
 
