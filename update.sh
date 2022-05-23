dfa50028ea7640ef34c99a3b8b20316c2a25f8ed
Ondřej Vaculík <35863250+vaculiko@users.noreply.github.com>
2022-05-23 11:00:41 +0200

    rpi-update -y flag removed

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Raspberry Pi Update"
 sudo apt update && sudo apt full-upgrade -y 
-sudo rpi-update -y
+sudo rpi-update
 sudo apt autoremove -y
 sudo apt autoclean -y
 sudo reboot
